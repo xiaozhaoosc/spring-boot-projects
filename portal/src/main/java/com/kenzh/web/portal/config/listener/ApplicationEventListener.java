23f70a817c4cfae7955f70fa725c50224f948311
zhaoxuan <Dev123456>
2018-07-03 11:42:54 +0800

    update

---
+++ portal/src/main/java/com/kenzh/web/portal/config/listener/ApplicationEventListener.java
@@ -0,0 +1,12 @@
+package com.kenzh.web.portal.config.listener;
+
+/**
+ * 类描述:
+ * 项目初始化的监听器
+ *
+ * @author zhaoxuan
+ * @create 2018-07-02 20:46
+ */
+public class ApplicationEventListener {
+
+}
---
+++ portal/src/main/java/com/kenzh/web/portal/controller/IndexController.java
@@ -0,0 +1,12 @@
+package com.kenzh.web.portal.controller;
+
+/**
+ * 类描述:
+ * 首页
+ *
+ * @author zhaoxuan
+ * @create 2018-07-02 20:52
+ */
+public class IndexController {
+
+}
